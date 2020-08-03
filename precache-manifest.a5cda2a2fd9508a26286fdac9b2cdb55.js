2985cf4b7bab1a5385183c301601d86dd53d73be
pjsaracho <uplift.pjsaracho@gmail.com>
2020-08-03 08:57:37 +0800

    Updates

---
+++ precache-manifest.a5cda2a2fd9508a26286fdac9b2cdb55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0925cfdb22300acbc81108ed54a2c8e",
+    "url": "/react-try/index.html"
+  },
+  {
+    "revision": "5e8a9c6a4d130e952082",
+    "url": "/react-try/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8ae721966f78cb6ad33",
+    "url": "/react-try/static/js/2.9983a655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-try/static/js/2.9983a655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8a9c6a4d130e952082",
+    "url": "/react-try/static/js/main.31dd6a8c.chunk.js"
+  },
+  {
+    "revision": "b6ba33d2798df83f8dab",
+    "url": "/react-try/static/js/runtime-main.333656cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-try/static/media/logo.5d5d9eef.svg"
+  }
+]);
